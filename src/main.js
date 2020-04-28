5c21c505689ec66b9919bff6743aa377ec709947
Camila Monreal <moncml@outlook.com>
2020-04-28 16:57:40 -0300

    main.js-print-character

--- src/main.js
+++ src/main.js
@@ -1,33 +1,14 @@
 import data from './data/rickandmorty/rickandmorty.js';
-//console.log(data);
-function listCharacters(characters){
-      let character;
-      let arrayCharacters=[];
-      for(character of characters){ 
-        arrayCharacters.push(character);
-      }
-      return arrayCharacters;
-    }
-  listCharacters(data.results);
-  let arrayCharacter = listCharacters(data.results);
 
-function printCharacter(character){
-  for (let [key, value] of Object.entries(character)){
-    console.log(key + " "+ value);  
+function printCharacter(characterList){
+  for (let character of characterList){
+    console.log(character.name); 
+    document.getElementById("characters").innerHTML += character.name + " ";
   }
   return;
 }
-printCharacter(arrayCharacter[0]);
+printCharacter(data.results);
 
-function setPositionCharacter(arrayCharacter){
-    let index;
-    let arrayCharcodes=[];
-    for(index in arrayCharacter){ 
-      arrayCharcodes.push(arrayCharacter.charCodeAt(index));
-      console.log(arrayCharcodes)
-    }
-    return arrayCharcodes;
-}
-setPositionCharacter(arrayCharacter); 
+
 
 
