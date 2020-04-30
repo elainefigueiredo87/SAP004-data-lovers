5a091935617cd747ff4eea98194dd17cb1c05e83
Camila Monreal <moncml@outlook.com>
2020-04-30 03:56:04 -0300

    css e main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import data from './data/rickandmorty/rickandmorty.js';
-
-function printCharacter(characterList){
-  for (let character of characterList){
-    console.log(character.name); 
-    document.getElementById("characters").innerHTML += character.name + " ";
-  }
-  return;
+//FAZER O LOOP NO DATA E RETORNAR UMA VARIAVEL COM O NOME DE CADA PROPRIEDADE DE CADA PERSONAGEM
+function getCharacters(characterList){
+	for (let character of characterList){
+	 	printCharacter(character);
+	}
 }
-printCharacter(data.results);
+getCharacters(data.results);
+//FUNÇÃO PRINTAR AS INFOS DO PERSONAGE, NO ITEM DA LISTA
+function printCharacter(character){
+	let listItem = createListItem(character);
+	listItem.innerHTML += "Status:" + " " + character.status + "<br /> ";
+	listItem.innerHTML += "Name:" + " " + character.name + "<br /> ";
+	listItem.innerHTML += "Specie:" + " " + character.species + "<br /> ";
+	listItem.innerHTML += "Type:"+ " " + character.type + "<br />";
+	listItem.innerHTML += "Gender:" + " " + character.gender + "<br />";
+	//listItem.innerHTML += "Episodes:" + " " + character.episode + "<br /> " ;
+	listItem.innerHTML += "Origin:" + " " + character.origin.name + "<br />";
+	//listItem.innerHTML += character.image + "<br /> ";
+}
+//CRIA O ITEM DA LISTA
+function createListItem(character){
+	let listItem = document.createElement("li");
+	document.getElementById("characters-list").appendChild(listItem);
+	return listItem;
+}
+
+//implementar lógica de episódios 
+// fazer com  que a img apareça 
+
