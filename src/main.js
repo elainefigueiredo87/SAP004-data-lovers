a0ba34e40a15087519b1df58a9dfa8acc505ad55
Camila Monreal <moncml@outlook.com>
2020-04-30 13:22:26 -0300

    tirar comentários

--- src/main.js
+++ src/main.js
@@ -1,24 +1,19 @@
 import data from './data/rickandmorty/rickandmorty.js';
-//FAZER O LOOP NO DATA E RETORNAR UMA VARIAVEL COM O NOME DE CADA PROPRIEDADE DE CADA PERSONAGEM
 function getCharacters(characterList){
 	for (let character of characterList){
 	 	printCharacter(character);
 	}
 }
 getCharacters(data.results);
-//FUNÇÃO PRINTAR AS INFOS DO PERSONAGE, NO ITEM DA LISTA
+
 function printCharacter(character){
 	let listItem = createListItem(character);
 	listItem.innerHTML += "Status:" + " " + character.status + "<br /> ";
 	listItem.innerHTML += "Name:" + " " + character.name + "<br /> ";
 	listItem.innerHTML += "Specie:" + " " + character.species + "<br /> ";
-	listItem.innerHTML += "Type:"+ " " + character.type + "<br />";
 	listItem.innerHTML += "Gender:" + " " + character.gender + "<br />";
-	//listItem.innerHTML += "Episodes:" + " " + character.episode + "<br /> " ;
 	listItem.innerHTML += "Origin:" + " " + character.origin.name + "<br />";
-	//listItem.innerHTML += character.image + "<br /> ";
 }
-//CRIA O ITEM DA LISTA
 function createListItem(character){
 	let listItem = document.createElement("li");
 	listItem.classList.add("character-information");
@@ -30,7 +25,6 @@ function createListItem(character){
 
 	return listItem;
 }
-
 //implementar lógica de episódios 
-// fazer com  que a img apareça 
+
 
