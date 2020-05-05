51f4dbfe62bd92833f62917c1d9894a7198f7117
elainefigueiredo87 <elaine.figueiredo87@gmail.com>
2020-05-05 14:08:35 -0300

    alteração css, inserção de campo para filtrar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import data from './data/rickandmorty/rickandmorty.js';
-function getCharacters(characterList){
-	for (let character of characterList){
-	 	printCharacter(character);
+function getCharacters(characterList) {
+	for (let character of characterList) {
+		printCharacter(character);
 	}
 }
 getCharacters(data.results);
 
-function printCharacter(character){
+function printCharacter(character) {
 	let listItem = createListItem(character);
 	listItem.innerHTML += "<br/> Status:" + " " + character.status + "<br /> ";
 	listItem.innerHTML += "Name:" + " " + character.name + "<br /> ";
@@ -14,17 +14,20 @@ function printCharacter(character){
 	listItem.innerHTML += "Gender:" + " " + character.gender + "<br />";
 	listItem.innerHTML += "Origin:" + " " + character.origin.name + "<br />";
 }
-function createListItem(character){
+function createListItem(character) {
 	let listItem = document.createElement("li");
 	listItem.classList.add("character-information");
 	document.getElementById("characters-list").appendChild(listItem);
-	let img = document.createElement('img'); 
+	let img = document.createElement('img');
 	img.classList.add("character-img")
 	img.src = character.image;
 	listItem.appendChild(img)
 
 	return listItem;
 }
-//implementar lógica de episódios 
+
+
+
+
 
 
